bf5e45906c6aca9d5ea3b8eb7bedd4413c4084d5
jesusvan <edivan.prof@gmail.com>
2019-04-22 00:06:13 -0300

    acertos 2

--- main.dart
+++ main.dart
@@ -1,33 +1,13 @@
 import 'cliente.dart';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import 'banco.dart';
-void main(){
-  var cliente = new Cliente();
-  var conta = new Conta();
-=======
-=======
->>>>>>> bd8f752e4fce0f4052f354f9102fd98d02991a74
 import 'caixaEletronico.dart';
 void main(){
   var cliente = new Cliente();
-  var operacoes = new Operacoes();
-<<<<<<< HEAD
->>>>>>> 1f43fea6ed9c7cb3ba7d13c41045d0f952d91a08
-=======
->>>>>>> bd8f752e4fce0f4052f354f9102fd98d02991a74
+  var conta = new Conta();
 
   cliente.titular = 'Eliezer Castro';
   cliente.conta = 11209;
   cliente.numConta =  '2553-8';
 
-<<<<<<< HEAD
-<<<<<<< HEAD
   conta.sacar();
-=======
-  operacoes.sacar();
->>>>>>> 1f43fea6ed9c7cb3ba7d13c41045d0f952d91a08
-=======
-  operacoes.sacar();
->>>>>>> bd8f752e4fce0f4052f354f9102fd98d02991a74
 }
